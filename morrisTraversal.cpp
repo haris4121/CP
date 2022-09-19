d35db6381242c3f9ccc4a5789c6a410bd4e71e53
haris rashid <harisrashid31@hariss-MacBook-Air.local>
2022-09-19 17:27:13 +0530

    morrris traversal

--- morrisTraversal.cpp
+++ morrisTraversal.cpp
@@ -1,53 +1,39 @@
-#include<iostream>
-using namespace std;
-#include<vector>
-
-
-
-
+class Solution {
 vector<int> getInorder(Node *root){
 vector<int> inorder;
-vector<int> preorder;
 Node * current = root;
-    while(current!=NULL){
-        if(current->left == NULL){
-            inorder.push_back(current->data);
-            preorder.push_back(current->data;)
-            current= current->right;
+while(current!=NULL){
+    if(current->left == NULL){
+        inorder.push_back(current->data);
+        current = current->right;
+    }
+    else{
+        Node *prev = current->left;
+        while(prev->right !=NULL && prev->right!=current){
+            prev = prev->right;
+        }
+        if(prev->right == NULL){
+            prev->right = current;
+            current= current->left;
         }
         else{
-            Node *prev = current->left;
-            while(prev->right && prev->right != current){
-                        prev = prev->right;
-            }
-            if(prev->right == NULL){
-                prev->right = current;
-                
-                preorder.push_back(current->data);
-                current = current->left;
-            }
-            else{
-                prev->right == NULL;
-                inorder.push_back(current->val);
-                current = current->right;
-            }
-
-
+            prev->right = NULL;
+            inorder.push_back(current->data);
+            current = current->right;
+        
         }
-
-
-
-
-    }
-
-
-return inoder;
-
-}
-
-int main()
-{
     
-
-    return 0;
+    }
+    
+    
 }
+return inorder;
+
+}
+    
+  public:
+    // Function to return a list containing the inorder traversal of the tree.
+    vector<int> inOrder(Node* root) {
+      return getInorder(root); 
+    }
+};
