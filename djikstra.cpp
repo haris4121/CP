9de7f036ad439594aa69b67e91d11b9647944388
haris rashid <harisrashid31@hariss-MacBook-Air.local>
2022-11-15 20:43:54 +0530

    updated djikstra

--- djikstra.cpp
+++ djikstra.cpp
@@ -1,16 +1,66 @@
-#include <bits/stdc++.h>
-typedef long long int ll;
-using namespace std;
-
-
-
-int main(){
-	ios::sync_with_stdio(0);
-    cin.tie(0);
-	
-	string str = "abxz";
-	// sort(str.begin(),str.end(),comp);
-	cout<<str<<endl;
-
-return 0;
-}
+class cmp{
+    public:
+    bool operator()(const pair<int,int> &p1,const pair<int,int> &p2){
+        if(p1.first != p2.first)
+            return p1.first > p2.first;
+        return p1.second > p2.second;
+    }
+    
+};
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int s)
+    {
+       vector<int> distance(V,INT_MAX);
+       distance[s] = 0;
+       
+       
+       priority_queue< pair<int,int>,vector<pair<int,int> >,cmp > pq;
+       
+       
+       pq.push({0,s});//weight ,distace
+       
+       
+       while(!pq.empty()){
+           
+           auto top = pq.top();
+           pq.pop();
+           
+            for(auto neighbour : adj[top.second])//gives vector as adj[top.second] gives vector of vector
+            {
+                int nextElement = neighbour[0];
+                int nextDistance = neighbour[1];
+                
+                if(distance[nextElement] > distance[top.second] + nextDistance)
+                {
+                    distance[nextElement] = distance[top.second] + nextDistance;
+                    pq.push({distance[nextElement],nextElement});
+                    
+                    
+                    
+                }
+                
+                
+                
+                
+                
+            }
+                
+                
+                
+           
+            
+           
+           
+           
+           
+       }
+       return distance;
+       
+       
+       
+    }
+};
