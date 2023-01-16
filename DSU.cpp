e5667be4868f57bc4e2606d87d5cf0274105f123
haris rashid <harisrashid31@hariss-MacBook-Air.local>
2023-01-16 18:09:27 +0530

    added DSU

--- DSU.cpp
+++ DSU.cpp
@@ -1,81 +1,49 @@
-#include<bits/stdc++.h>
-using namespace std;
+class DSU {
+    vector<int> rank, parent, size; 
+public: 
+    DSU(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
 
-class DSU{
-private:
-	vector<int> rank;
-	vector<int> size;
-	vector<int> parant;
-public:
-		DSU(int n){
-			rank.resize(n+1,0);
-			size.resize(n+1,1);
-			parant.resize(n);
-			for(int i  = 0 ;i<=n;i++)
-				parant[i] = i;
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
 
-		}
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
 
-		int findUltimateParant(int node){ // takes 0(alpha) == 0(1)
-			if(node == parant[node])
-				return node;
-
-			return parant[node] = findUltimateParant(parant[node]); // path compression step ,backtracking step
-
-		}
-
-
-		void unionBySize(int u,int v){
-			int uParantOfU = findUltimateParant(u);
-			int uParantOfV = findUltimateParant(v);
-			if(uParantOfV == uParantOfU)
- 				return ; // means if they are already in same component
-
-			if(size[uParantOfU] < size[uParantOfV]){
-				parant[u] = v;
-				size[v] += size[u];
-
-			}
-			else{
-				parant[v] = u;
-				size[u] += size[v];
-
-			}
-
-		}
-
-		void unionByRank(int u,int v){
-			int uParantOfU = findUltimateParant(u);
-			int uParantOfV = findUltimateParant(v);
-			if(uParantOfV == uParantOfU)
- 				return ; // means if they are already in same component
-
-			if(size[uParantOfU] < size[uParantOfV]){
-				parant[u] = v;
-				rank[v] += 1;
-
-			}
-			else{
-				parant[v] = u;
-				rank[u] += 1;
-
-			}
-
-		}
-
-
-};
-
-int main(){
-DSU dsu(7);
-dsu.unionBySize(1,2);
-dsu.unionBySize(4,3);
-dsu.unionBySize(5,6);
-dsu.unionBySize(1,7);
-
-cout<<dsu.findUltimateParant(2)<<" "<<dsu.findUltimateParant(5);
-
-
-
-
-}
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
